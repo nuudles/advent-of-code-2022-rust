e542bab26086265b354e255e9b7dec6bd1f82027
Christopher Luu <nuudles@gmail.com>
2022-12-01 11:46:17 +0000

    Add Day 1 solution

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,31 @@
-pub fn part1(input: String) {}
+use itertools::Itertools;
 
-pub fn part2(input: String) {}
+use crate::selfprint::SelfPrint;
+
+pub fn part1(input: String) {
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|l| l.parse::<u64>().unwrap_or_default())
+                .sum::<u64>()
+        })
+        .max()
+        .unwrap_or_default()
+        .print();
+}
+
+pub fn part2(input: String) {
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|l| l.parse::<u64>().unwrap_or_default())
+                .sum::<u64>()
+        })
+        .sorted()
+        .rev()
+        .take(3)
+        .sum::<u64>()
+        .print();
+}
